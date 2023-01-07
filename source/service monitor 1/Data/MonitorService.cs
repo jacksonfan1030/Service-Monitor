b88bc19173fc2e83fd1b1a61fa313a4eb703eb61
jacksonfan1030 <fa04962@gmail.com>
2023-01-07 17:12:34 +0800

    code

---
+++ source/service monitor 1/Data/MonitorService.cs
@@ -0,0 +1,17 @@
+﻿namespace service_monitor_1.Data
+{
+    public class MonitorService
+    {
+        public string DoMonitor(string url)
+        {
+            //呼叫功能類別
+            ServiceMonitor servicemonitor = new ServiceMonitor();
+
+            //進行偵測
+            string result = servicemonitor.Monitor(url);
+
+            //回傳結果
+            return result;
+        }
+    }
+}
---
+++ source/service monitor 1/Data/ServiceMonitor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.ComponentModel;
+using System.Threading;
+
+namespace service_monitor_1.Data
+{
+    public class ServiceMonitor
+    {
+        public string Monitor(string url)
+        {
+            //偵測主程式
+            // Create a request for the URL.
+            WebRequest request = WebRequest.Create(url);
+            // If required by the server, set the credentials
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Display the status.
+            Console.WriteLine(response.StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream datastream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(datastream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            string response1 = response.StatusDescription;
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Cleanup the streams and the response.
+            reader.Close();
+            datastream.Close();
+            response.Close();
+            return response1;
+            //return responseFromServer;
+
+        }
+    }
+}
